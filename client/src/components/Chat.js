74c3a68ced97ad430aa3f5b6ab91b15a2b79a159
Gulshan Saini <gulshan.saini@gmail.com>
2020-09-07 15:07:30 +0530

    fixed issue if no one available for chat

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -26,6 +26,8 @@ const Chat = () => {
 
     // listen for users from server
     socket.on("users", (socketUsers) => {
+      console.log(socketUsers);
+      if (socketUsers.length === 0) return;
       // filter current user
       const filteredUser = socketUsers.filter((u) => {
         return u._id !== _id;
@@ -176,11 +178,10 @@ const Chat = () => {
         <div className="border-b flex px-6 py-2 items-center flex-none">
           <div className="flex flex-col">
             <h3 className="text-grey-darkest mb-1 font-extrabold">
-              #{chatUser.name}
+              {chatUser.name
+                ? `#${chatUser.name}`
+                : "No user available for chat"}
             </h3>
-            <div className="text-grey-dark text-sm truncate">
-              Chit-chattin' about ugly HTML and mixing of concerns.
-            </div>
           </div>
           <div className="ml-auto hidden md:block">
             <div className="relative">
